3c7499c820eca22e8c97b9afd8b2a9d197e532f7
Aditya Gupta <adityagupta765469@gmail.com>
2022-01-03 22:26:15 +0530

    Time: 8 ms (10.30%), Space: 41.7 MB (17.76%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,21 +16,20 @@
 class Solution {
     int idx = 0;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-       TreeNode node = buildTreeHelp(preorder, inorder, 0, inorder.length-1);
+      TreeNode node = buildTreeHelp(preorder, inorder, 0, preorder.length-1);
         return node;
     }
-    TreeNode buildTreeHelp(int[] preorder, int[] inorder, int start, int end){
-        if(start > end) return null;
-        System.out.println(idx);
-        int curr = preorder[idx];
-        idx++;
-        TreeNode node = new TreeNode(curr);
-        if(start == end)    return node;
-        int pos = search(inorder, start, end, curr);
-        node.left = buildTreeHelp(preorder, inorder, start, pos-1);
-        node.right = buildTreeHelp(preorder, inorder, pos+1, end);
-        return node;
-    }
+   TreeNode buildTreeHelp(int[] preorder, int[] inorder, int start, int end){
+       if(start > end)  return null;
+       int curr = preorder[idx];
+       idx++;
+       TreeNode node = new TreeNode(curr);
+       if(start == end) return node;
+       int pos = search(inorder, start, end, curr);
+       node.left = buildTreeHelp(preorder, inorder, start, pos-1);
+       node.right = buildTreeHelp(preorder, inorder, pos+1, end);
+       return node;
+   }
     int search(int[] inorder, int start, int end, int curr){
         for(int i = start; i <= end; i++)
             if(inorder[i] == curr)
