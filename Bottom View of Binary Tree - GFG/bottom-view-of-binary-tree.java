2da28ba40b37b91a8f8c0a18c10e432d8165f5af
Aditya Gupta <adityagupta765469@gmail.com>
2022-02-26 23:52:10 +0530

    Added solution - LeetHub

---
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.java
@@ -0,0 +1,161 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+//Contributed by Sudarshan Sharma
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node
+{
+    int data; //data of the node
+    int hd; //horizontal distance of the node
+    Node left, right; //left and right references
+ 
+    // Constructor of tree node
+    public Node(int key)
+    {
+        data = key;
+        hd = Integer.MAX_VALUE;
+        left = right = null;
+    }
+}
+
+
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	        Solution ob = new Solution();
+			    ArrayList <Integer> res = ob.bottomView(root);
+			    for (Integer num : res) System.out.print (num + " ");
+		     	System.out.println();
+	        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Pair<U, V>
+{
+    public final U first;       // first field of a pair
+    public final V second;      // second field of a pair
+ 
+    // Constructs a new Pair with specified values
+    private Pair(U first, V second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+ 
+    // Factory method for creating a Typed Pair immutable instance
+    public static <U, V> Pair <U, V> of(U a, V b)
+    {
+        // calls private constructor
+        return new Pair<>(a, b);
+    }
+}
+class Solution
+{
+    //Function to return a list containing the bottom view of the given tree.
+    public ArrayList <Integer> bottomView(Node root)
+    {
+        // Code here
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root == null)
+        return list;
+        Map<Integer, Pair<Integer, Integer>> mp = new TreeMap<>();
+        printBottom(root, 0, 0, mp);
+        
+        for(Pair<Integer, Integer> it : mp.values())
+            list.add(it.first);
+        return list;
+    }
+    void printBottom(Node root, int dist, int level, Map<Integer, Pair<Integer, Integer>> mp){
+        if(root == null)
+        return;
+        if(!mp.containsKey(dist) || level >= mp.get(dist).second)
+            mp.put(dist, Pair.of(root.data, level));
+        printBottom(root.left, dist-1, level+1, mp);
+        printBottom(root.right, dist+1, level+1, mp);
+    }
+}
