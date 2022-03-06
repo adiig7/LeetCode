eaad903047fe446a36e5fdf2111a900dfd8aa9c8
Aditya Gupta <adityagupta765469@gmail.com>
2022-03-06 13:15:01 +0530

    Time: 18 ms (12.52%), Space: 74.5 MB (7.10%) - LeetHub

--- 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
+++ 894-all-possible-full-binary-trees/894-all-possible-full-binary-trees.java
@@ -18,25 +18,25 @@ class Solution {
         List<TreeNode> res = helper(n);
         return res;
     }
-    List<TreeNode> helper(int n){
-        if(n == 1){
-            List<TreeNode> ans = new ArrayList<>();
-            ans.add(new TreeNode(0));
-            return ans;
-        }
-        List<TreeNode> res = new ArrayList<>();
-        for(int i = 1; i < n; i+=2){
-            List<TreeNode> left = helper(i);
-            List<TreeNode> right = helper(n-i-1);
-            for(TreeNode l : left){
-                for(TreeNode r: right){
-                    TreeNode root = new TreeNode(0);
-                    root.left = l;
-                    root.right = r;
-                    res.add(root);
-                }
-            }
-        }
-        return res;
-    }
+  List<TreeNode> helper(int n){
+      if(n == 1){
+          List<TreeNode> temp = new ArrayList<>();
+          temp.add(new TreeNode(0));
+          return temp;
+      }
+      List<TreeNode> res = new ArrayList<>();
+      for(int i = 1; i < n; i+=2){
+          List<TreeNode> left = helper(i);
+          List<TreeNode> right = helper(n-i-1);
+          for(TreeNode l: left){
+              for(TreeNode r: right){
+                  TreeNode root = new TreeNode(0);
+                  root.left = l;
+                  root.right = r;
+                  res.add(root);
+              }
+          }
+      }
+      return res;
+  }
 }
